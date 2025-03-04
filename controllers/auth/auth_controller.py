564c4ea53edcf9a858d8772de0d8f952367f0576
Jimia18 <jimiaali5@gmail.com>
2025-03-04 14:22:57 +0300

    update

--- controllers/auth/auth_controller.py
+++ controllers/auth/auth_controller.py
@@ -1,4 +1,4 @@
-# Handles user authentication
+# Handles user authenticatio
 from flask import Blueprint, request, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from models import User, db

e744e07db7967af4bf23aa5c5965e38bb7cf517a
Hawezishakirah <hawezishakirah@gmail.com>
2025-04-03 11:39:17 +0300

    Update extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -1,6 +1,6 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_bcrypt import Bcrypt
+from flask import SQLAlchemy
+from flask import Migrate
+from flask import Bcrypt
 from flask_jwt_extended import JWTManager
 
 
